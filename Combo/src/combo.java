7c47c223e89a2210223234423c294027e2e9553d
Jalisa <Jalisa@128.4.131.178>
2015-04-28 16:38:16 -0400

    2

--- Combo/src/combo.java
+++ Combo/src/combo.java
@@ -1,6 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
+import javax.swing.ComboBoxModel;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,9 +15,16 @@ public class combo extends JFrame {
 	private static final long serialVersionUID = 1L;
 	String[] months = { "1, January", "2, February", "3, March", "4, April",
 			"5, May", "6, June", "7, July", "8, August", "9, " + "September",
-			"10, October", "11, November", "12, December" };
+			"10, October", "11, November", "12, December"}
+	String[] days = { "1" ,"2", "3", "4",
+			"5", "6", "7", "8", "9, " ,
+			"10", "11", "12", "13","14","15","16","17","18","19","20","21",
+			"22","23","24","25","26","27","28","29","30","31"};
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	 JComboBox monthlist= new JComboBox(months);
+	private ComboBoxModel date;
+	@SuppressWarnings("rawtypes")
+	JComboBox datelist= new JComboBox(date);
 
 	// Labels created for JFrame
 	JLabel monthlabel = new JLabel("Month");
